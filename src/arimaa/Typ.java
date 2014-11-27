1fb10b14adfd9d7c3597f1a86830dfa19343ef5d
LePenseur13 <josef.hanifl@gmx.at>
2014-11-27 21:02:53 +0100

    extra Klasse für Typ und Farbe

---
+++ src/arimaa/Typ.java
@@ -0,0 +1,36 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package arimaa;
+
+/**
+ *
+ * @author JosefChristoph, Alexander Holzinger
+ * @version 1.0
+ */
+enum Typ {
+    Kaninchen, Katze, Hund, Pferd, Kamel, Elefant;
+
+    
+    
+    /**
+     * Gibt den Unterschied der Stärken, der beiden Typen an
+     * 
+     * @param t Typ mit dem verglichen wird
+     * @return 
+     */
+    public int compare( Typ t ) {
+        return this.ordinal() - t.ordinal();
+    }
+    
+    /**
+     * 
+     * @param t Typ mit dem verglichen wird
+     * @return 
+     */
+    public boolean isStronger( Typ t ) {
+        return this.compare( t ) > 0;
+    }
+}
